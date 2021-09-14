d3dfdf8cdc8bcea583f56d66a6be594d0a22b951
Priti Solanki <priti@noemail.com>
2021-09-13 23:08:43 -0400

    Fix : Fix the payment workflow

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars')
-const stripe = require('stripe');
+//const stripe = require('stripe');
+const stripe = require('stripe')('sk_test_tZemrYLxgDc62ml2VrguGO7I00C32NlAHz');
+
 
 var app = express();
+app.use(express.urlencoded({extended: true})); 
+app.use(express.json());
 
 // view engine setup (Handlebars)
 app.engine('hbs', exphbs({
@@ -58,7 +62,52 @@ app.get('/checkout', function(req, res) {
  * Success route
  */
 app.get('/success', function(req, res) {
-  res.render('success');
+  res.status(200);
+  res.setHeader('Content-Type', 'text/html');
+  res.render('success',{
+    id: req.query.id,
+    productname : req.query.item,
+    amount : parseInt(req.query.amt).toFixed(2)
+  });
+});
+
+
+const calculateOrderAmount = (productItems) => {
+  // calculate total order amounts in cents
+  let orderTotal = 0;
+  for(items of productItems)
+  {
+    orderTotal += items.amt;
+  }
+  return orderTotal * 100;
+};
+
+app.post("/create-payment-intent", async (req, res) => {
+  //get orderTotal
+  const orderTotal = calculateOrderAmount(req.body);
+  if(orderTotal == undefined){
+    res.send({
+      error : "Item price is missing"
+    });
+  }
+
+  // Create a PaymentIntent with the order amount and currency
+  try
+  {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: orderTotal,
+      currency: "usd",
+      payment_method_types: ['card']
+    });
+    res.send({
+      clientSecret: paymentIntent.client_secret
+    });
+  }catch(e){
+    console.log(e);
+    res.send({
+      error: e.message
+    })
+  }
 });
 
 /**
--- public/js/custom.js
+++ public/js/custom.js
@@ -1,14 +1,101 @@
 /**
  * Clientside helper functions
  */
+//Initialize Stripe sdk with your account Publishable key
+var stripe = Stripe('pk_test_6N6wZquvh3KxaPCwBjxNydJ700foQbuwZv');
+var elements = stripe.elements();
+var style = {
+  base: {
+    color: "#8E44AD",
+  }
+};
+
 
 $(document).ready(function() {
-  var amounts = document.getElementsByClassName("amount");
 
+  var amounts = document.getElementsByClassName("amount");
+  var orderTotal = 0 ;
   // iterate through all "amount" elements and convert from cents to dollars
-  for (var i = 0; i < amounts.length; i++) {
+  for (var i = 0; i < amounts.length-1; i++) {
     amount = amounts[i].getAttribute('data-amount') / 100;  
     amounts[i].innerHTML = amount.toFixed(2);
+    orderTotal += parseInt(amount.toFixed(2));
   }
-})
+  amounts[i].innerHTML = amount.toFixed(2);
 
+  //prepare purchase 
+  const urlParams = new URLSearchParams(window.location.search);
+  const itemId = urlParams.get('item');
+
+  var purchase = {
+    items: [{ "id": itemId , "amt": orderTotal, "name":$("#purchase_item_title").text().trim()}]
+  };
+  
+  //Attach stripe call element
+  if(document.getElementById("card-element") != null)
+  {
+    var card = elements.create("card", { style: style });
+    card.mount("#card-element");
+
+    card.on('change', ({error}) => {
+      let displayError = document.getElementById('card-errors');
+      if (error) {
+        displayError.textContent = error.message;
+      } else {
+        displayError.textContent = '';
+      }
+    });
+  }
+  
+  //attach the submit event to paymentform to submit the transaction
+  var paymentform = document.getElementById('payment-form');
+  paymentform && paymentform.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    //fetch paymentIntent token from Stripe
+    paymentSecret = (async () => {
+        try{
+          const response = await fetch('/create-payment-intent',{
+            method: 'POST',
+            body: JSON.stringify(purchase.items),
+            headers: { 'Content-Type': 'application/json' }
+          });
+          const {clientSecret: clientSecret} = await response.json();
+          return clientSecret;
+        }catch(error){
+          console.log(error.message)
+        }
+    })();
+
+    //submit the card details with secret to Strip for transaction processing
+    paymentSecret.then(function(client_secret){
+      stripe.confirmCardPayment(client_secret, {
+        payment_method: {
+          card: card,
+          billing_details: {
+            name: 'test transaction'
+          }
+        }
+      }).then(function(result) {
+        if (result.error) {
+          carderrElement = document.getElementById('card-errors'); 
+          carderrElement.innerHTML = result.error.message;
+        } else {
+          // The payment has been processed!
+          if (result.paymentIntent.status === 'succeeded') {
+            productname = $("#purchase_item_title").text().trim();
+            urlRedirect = `/success?id=${result.paymentIntent.id}&item=${productname}&amt=${orderTotal}`
+            const htmlContent = fetch(urlRedirect,{
+              method: 'GET'
+            });
+            htmlContent.then((response)=>{
+              window.location.href=response.url;
+            })
+          }
+        }
+      });
+    })
+    .catch(error =>{
+      throw e;
+    });
+  });
+});
